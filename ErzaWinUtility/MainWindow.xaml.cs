0f8c3c3729368ff720e49fe17b07ca22a9351255
9Erza <erykzawlocki@gmail.com>
2026-04-12 22:57:27 +0200

    zmiany

--- ErzaWinUtility/MainWindow.xaml.cs
+++ ErzaWinUtility/MainWindow.xaml.cs
@@ -1,24 +1,32 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ErzaWinUtility.MVVM.Views;
 
 namespace ErzaWinUtility
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private static MainWindow _instance;
+
         public MainWindow()
         {
             InitializeComponent();
+            _instance = this;
+            MainFrame.Content = new OptimizeView(); // Widok startowy
+            Log("SYSTEM", "New project core initialized.");
         }
+
+        public static void Log(string tag, string message)
+        {
+            _instance?.Dispatcher.Invoke(() => {
+                _instance.LogTerminal.AppendText($"[{DateTime.Now:HH:mm:ss}] [{tag}] {message}\n");
+                _instance.LogTerminal.ScrollToEnd();
+            });
+        }
+
+        private void NavOptimize_Click(object sender, RoutedEventArgs e) => MainFrame.Content = new OptimizeView();
+        private void NavConfig_Click(object sender, RoutedEventArgs e) => MainFrame.Content = new ConfigView();
+        private void NavInstall_Click(object sender, RoutedEventArgs e) => MainFrame.Content = new InstallView();
+        private void Exit_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
     }
 }
